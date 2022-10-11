e159e802dd695208a86cbf1b4dd31fb1b419464b
Jeewoo Jung <35914056+koharin@users.noreply.github.com>
2022-10-11 15:25:52 +0900

    Update coverage_graph.bat

--- winafl_coverage/coverage_graph.bat
+++ winafl_coverage/coverage_graph.bat
@@ -4,13 +4,12 @@ SET arg2=%2
 
 :: GET TARGET OFFSET
 FOR /F %%i IN ('C:\Users\johan\code\offset\build\bin\Release\offset.exe %arg1% %arg2%') DO set OFFSET=%%i
-
 ECHO ON
 
 cd C:\Users\johan\winafl\build32\bin\Release
 
 :: GET COVERAGE DATA OF TESTCASES
-afl-showmap2.exe -o data -D "C:\Users\johan\DynamoRIO-Windows-9.0.19272\bin32" -- -coverage_module libcrypto-3.dll -target_module openssl.exe -target_offset %OFFSET% -nargs 1 -- "C:\Users\johan\code\openssl\build\bin\Release\openssl.exe" C:\Users\johan\code\openssl
+afl-showmap2.exe -o data -D "C:\Users\johan\DynamoRIO-Windows-9.0.19272\bin32" -- -coverage_module libcrypto-3.dll -target_module openssl2.exe -target_offset %OFFSET% -nargs 2 -- "C:\Users\johan\code\openssl2\build\bin\Release\openssl2.exe" C:\Users\johan\code\openssl2
 
 :: SHOW COVERAGE GRAPH
-python C:\Users\johan\code\graph.py C:\Users\johan\code\openssl\coverage\coverage
+python C:\Users\johan\code\graph.py C:\Users\johan\code\openssl2\coverage\coverage
